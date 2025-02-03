87fc55e50ac593a2784da414022192525fd5c991
Diogo Alves <diogofalves05@gmail.com>
2025-02-03 12:38:10 -0500

    Changed "Sun" Black/White Color To White

--- Picture.java
+++ Picture.java
@@ -85,7 +85,7 @@ public class Picture
         wall.changeColor("black");
         window.changeColor("white");
         roof.changeColor("black");
-        sun.changeColor("black");
+        sun.changeColor("white");
     }
 
     /**
